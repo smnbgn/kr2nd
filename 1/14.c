dcc37a3263995c2ee85013908bb33547e8617d89
Simon Bogina <simon@bogina.si>
2024-12-26 23:19:24 +0100

    Add exercise 14

--- 1/14.c
+++ 1/14.c
@@ -1,3 +1,30 @@
 /* Exercise 1-14. Write a program to print a histogram of the frequencies 
 of different characters in its input.*/
 
+#include <stdio.h>
+
+#define ASCII_SIZE 128
+
+main()
+{
+    int c, i, j;
+    int characters[ASCII_SIZE];
+
+    for (i = 0; i < ASCII_SIZE; i++)
+        characters[i] = 0;
+
+    while((c = getchar()) != EOF)
+    {
+        ++characters[c];
+    }
+
+    for (i = 0; i < ASCII_SIZE; i++) {
+        if (isprint(i)) {
+            printf("(%c) ", i);
+            for (j = 0; j < characters[i]; j++)
+                putchar('*');
+            printf("\n");
+        }
+    }
+
+}
